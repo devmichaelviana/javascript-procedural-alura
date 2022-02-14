1388ec3bf22f97c975aef22430fa8b9da8c5449e
Michael Viana <vmichael116@gmail.com>
2022-02-14 14:04:29 -0300

    Parte procedural de JS

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,9 @@
+//Console.log("Introdução à variáveis");
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+const idade = 29;
+const nome = "Ricardo";
+
+//não fazer
+//mes = "Janeiro";
---
+++ 4-conversaoDeTipos.js
@@ -0,0 +1,8 @@
+// console.log("Trabalhando com conversão de tipos");
+
+console.log("ano" + 2020);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+console.log("7" / "2");
+console.log("Michael" / "2");
+console.log(7.6 / 2);
